62eb38509a68f5e036e3056944190ecf7d62d9d5
brandon <bwp44f@mst.edu>
2018-03-26 18:47:42 -0500

    moved main into main

--- data_models.py
+++ data_models.py
@@ -1,4 +1,4 @@
-from data_gathering import get_recipe_info, get_item_max_buy_price, get_item_min_sell_price, get_item_info, get_recipe_ids, timed_cache_data, get_recipe_max_buy_price, get_known_recipes
+from data_gathering import get_recipe_info, get_item_max_buy_price, get_item_min_sell_price, get_item_info, get_recipe_ids, timed_cache_data, get_recipe_max_buy_price
 
 from graphviz import Digraph
 
@@ -8,73 +8,6 @@ import json
 from collections import defaultdict
 from itertools import permutations
 from enum import Enum
-
-
-class Item():
-    def __init__(self, id):
-        self.item_id = id
-        self.name = None
-        self.recipe_id = 0
-        self._load()
-
-    def save(self):
-        with open('items/{}'.format(self.item_id), 'w') as writer:
-            writer.write(json.dumps({'id' : self.item_id,
-                                     'name' : self.name,
-                                     'recipe_id' : self.recipe_id}))
-
-    def _load(self):
-        if not os.path.isdir('items'):
-            os.makedirs('items')
-
-        if os.path.isfile('items/{}'.format(self.item_id)):
-            with open('items/{}'.format(self.item_id), 'r') as reader:
-               item_info = json.loads(reader.read())
-        else:
-            print("getting item: {}".format(self.item_id))
-            item_info = get_item_info(self.item_id)
-            item_info['recipe_id'] = 0
-
-            with open('items/{}'.format(self.item_id), 'w') as writer:
-                writer.write(json.dumps(item_info))
-
-        if item_info:
-            self.item_id = item_info['id']
-            self.name = item_info['name']
-            self.recipe_id = item_info['recipe_id']
-
-        #pprint(item_info)
-    def __str__(self):
-        return self.name
-
-class Recipe():
-    def __init__(self, id):
-        self.recipe_id = id
-        self.output_id = 0
-        self.input_info = []
-        self.mhload()
-
-    def mhload(self):
-        if not os.path.isdir('recipes'):
-            os.makedirs('recipes')
-        
-        if os.path.isfile('recipes/{}'.format(self.recipe_id)):
-            with open('recipes/{}'.format(self.recipe_id), 'r') as reader:
-                recipe_info = json.loads(reader.read())
-        else:
-            recipe_info = get_recipe_info(self.recipe_id)
-            
-            with open('recipes/{}'.format(self.recipe_id), 'w') as writer:
-                writer.write(json.dumps(recipe_info))
-
-        if recipe_info:
-            self.output_id = recipe_info['output_item_id']
-            self.input_info = recipe_info['ingredients']
-
-    def __str__(self):
-        val = "RECIPE\n"
-        val += "\tOutput Id: {}\n".format(self.output_id)
-        return val
 
 @timed_cache_data(10  * 60 * 60)
 def get_item_recipe(item_id):
@@ -89,13 +22,9 @@ def get_item_recipe(item_id):
         # no recipe to make the item, thus return the item
         return (item_id, 'item')
 
-LoadedRecipeId = 0
 
 class LoadedRecipe():
     def __init__(self, id):
-        global LoadedRecipeId
-        self.my_id = LoadedRecipeId
-        LoadedRecipeId += 1
         self.recipe_id = id
         recipe_info = get_recipe_info(self.recipe_id)
         self.output_id = recipe_info['output_item_id']
@@ -137,46 +66,6 @@ def build_dep_graph(parent, dep_id_graph, parent_ids, recipe_id_map):
             build_dep_graph(i['value'], dep_id_graph, parent_ids, recipe_id_map)
         else:
             dep_id_graph[str(parent.recipe_id)][str(i['value'])] = True
-
-def children_buy_price(parent):
-    t = 0
-    for i in parent.input_info:
-        if type(i['value']) == LoadedRecipe:
-            t += i['count'] * get_recipe_max_buy_price(i['value'].recipe_id)
-        else:
-            t += i['count'] * get_item_max_buy_price(i['value'])
-    return t
-
-def children_frontier_price(frontier):
-    t = 0
-    for i in frontier:
-        if type(i['value']) == LoadedRecipe:
-            t += i['count'] * get_recipe_max_buy_price(i['value'].recipe_id)
-        else:
-            t += i['count'] * get_item_max_buy_price(i['value'])
-    return t
-
-def already_have_frontier(values, frontier):
-    for i in values:
-        values_equal = True
-        for j in frontier:
-            if j not in i:
-                values_equal = False
-                break
-        if values_equal:
-            return True
-            break
-    return False
-                
-def get_frontiers(current_frontier, values):
-    #if not already_have_frontier(values, current_frontier):
-    values.append(current_frontier)
-    for combo in current_frontier:
-        new_combo = list(current_frontier)
-        new_combo.remove(combo)
-        if type(combo['value']) == LoadedRecipe:
-            get_frontiers(combo['value'].input_info + new_combo, values)
-
 
 def get_recipe_min_make_price(recipe):
     t = 0
@@ -281,55 +170,47 @@ class Dir(Enum):
 def create_table(recipe_id, table):
     recipe_info = get_recipe_info(recipe_id)
     recipe_item_info = get_item_info(recipe_info['output_item_id'])
-    print('\nRecipe output: {}'.format(recipe_item_info['name']))
     l = LoadedRecipe(recipe_id)
     k = find_max(l)
-    print(k)
     for i in k[1]:
-        print(i)
         if type(i['value']) == LoadedRecipe:
+            meta_i = i
             i = i['value']
             item_info = get_item_info(i.output_id)
-            print("{}: {}".format(item_info['name'], i))
             if item_info['name'] in table.keys():
-                print("Already have {}".format(item_info['name']))
+                pass
             else:
+                table[item_info['name']] = {'action' : 'Sell',
+                                            'count' : meta_i['count'],
+                                            'value' : meta_i['coinage']}
                 for j in i.input_info:
                     if type(j['value']) == LoadedRecipe:
                         item_info_id = j['value'].output_id
                     else:
                         item_info_id = j['value']
                     ing_item_info = get_item_info(item_info_id)
-                    table[ing_item_info['name']] = {'action' : 'Material/Buy',
-                                                    'parent' : recipe_item_info['name'],
-                                                    'value' : j['coinage'],
-                                                    'count' : j['count']}
-                    print(j)
-                    print(ing_item_info)
+                    if ing_item_info['name'] in table.keys():
+                        prev_vablue = table[ing_item_info['name']]
+                        if prev_vablue['action'] == 'Sell':
+                            table[ing_item_info['name']] = {'action': 'Material/Buy',
+                                                            'parent': recipe_item_info['name'],
+                                                            'value': j['coinage'],
+                                                            'count': j['count']}
+                        else:
+                            pass
+                    else:
+                        table[ing_item_info['name']] = {'action': 'Material/Buy',
+                                                        'parent': recipe_item_info['name'],
+                                                        'value': j['coinage'],
+                                                        'count': j['count']}
         else:
             item_info = get_item_info(i['value'])
-            print("{}: {}".format(item_info['name'], i))
             if item_info['name'] in table.keys():
-                print("Already selling: {}".format(item_info['name']))
-                print(k[0])
+                pass
             else:
                 table[item_info['name']] = {'action': 'Sell',
                                             'parent' : recipe_item_info['name'],
                                             'value' : i['coinage'],
                                             'count' : i['count']}
 
-if __name__ == "__main__":
-    table = {}
-    count = 0
-    max_count = 40
-    #for i in range(2, 700):
-    #    k = LoadedRecipe(i)
-    #    dot = Digraph(comment="Buy")
-    #    graph_recipe(k, dot, Dir.Buy)
-    #    dot.render('buy_{}'.format(k.output_id), view=True)
-    #    create_table(i, table)
-
-    for charac, recipe_num in get_known_recipes():
-        create_table(recipe_num, table)
-    pprint(table)
 
--- main.py
+++ main.py
@@ -1,149 +1,70 @@
 
-
-from data_gathering import GW2Request, Uri, get_recipe_ids
-from data_gathering import get_item_min_sell_price, get_recipe_max_buy_price
-from data_gathering import get_characters, get_character_crafting, get_character_recipes
-from data_gathering import get_known_recipes, get_all_items, get_item_max_buy_price
-from data_models import Recipe, Item
-
-from graphviz import Digraph
-
-import pdb
-import os
+from data_gathering import get_known_recipes, get_item_info, get_recipe_info, get_item_max_buy_price
+from data_models import LoadedRecipe
 
 from pprint import pprint
-from functools import lru_cache
 
-all_items_by_id = {}
+item_max_buy_table = {}
 
-def load_all_items():
-    print("Loading Items")
-    for i in range(1000):
-        all_items_by_id[str(i)] = Item(i)
-
-@lru_cache(maxsize=40000)
-def get_item_recipe(item_id):
-    i = Item(item_id)
-    #print("Get item {} recipe: {}".format(i.name, i.recipe_id))
-    if int(i.recipe_id) == -1: # thus we have no recipe for item
-        return None
-    elif int(i.recipe_id) == 0:
-        for j in get_recipe_ids():
-            recp = Recipe(j)
-            if recp.output_id == item_id:
-                i.recipe_id = j
-                i.save()
-                return recp
-        # couldn't find recipe thus we'll mark down there isn't one
-        if i.recipe_id == 0:
-            i.recipe_id = -1
-            i.save()
-    else:
-        return Recipe(i.recipe_id)
-
-def account_has_item(item_id):
-    character_items = get_all_items()
-    for char_name, item in character_items:
-        if item == item_id:
-            return True
-    return False
-
-def get_item_make_price(item_id, recipe_listing, dot):
-    recipe = get_item_recipe(item_id)
-    #todo: check inventory for mats and reduce price etc
-    if account_has_item(item_id):
-        return 0
-    elif recipe is None:
-        return get_item_min_sell_price(item_id)
-    else:
-        return calculate_cost_profit(recipe.recipe_id, recipe_listing, dot)
-
-def calculate_cost_profit(recipe_id, recipe_listing, dot):
-    recipe = Recipe(recipe_id)
-    cost = 0
-    item_t = Item(recipe.output_id)
-    for j in recipe.input_info:
-        recipe_list = []
-        graph_details = []
-        itm = Item(j['item_id'])
-        min_sell_price = get_item_min_sell_price(j['item_id'])
-        make_price = get_item_make_price(j['item_id'], recipe_list, graph_details)
-        cost += j['count']  * min(min_sell_price, make_price)
-        edge_info = {'edge' : [itm.name, item_t.name]}
-        if min_sell_price <= make_price:
-            recipe_listing.append({'buy' : itm.name, 'cost' : min_sell_price})
-            # dot.node(itm.name, color='red')
-            edge_info['color'] = 'red'
-            edge_info['cost'] = str(min_sell_price)
+def create_table(recipe_id, table):
+    recipe_info = get_recipe_info(recipe_id)
+    recipe_item_info = get_item_info(recipe_info['output_item_id'])
+    l = LoadedRecipe(recipe_id)
+    max_ingre = find_max(l, item_max_buy_table)
+    for sub_ing in max_ingre[1]:
+        print(sub_ing)
+        if type(sub_ing['value']) == LoadedRecipe:
+            pass
         else:
-            # dot.node(itm.name, color='green')
-            edge_info['color'] = 'green'
-            recipe_listing.append({'make':  itm.name, 'cost' : make_price})
-            recipe_listing.extend(recipe_list)
-            dot.extend(graph_details)
-        dot.append(edge_info)
-        #dot.edge(itm.name, item_t.name, label=str(j['count']))
+            item_info = get_item_info(sub_ing['value'])
+            if item_info['name'] in table.keys():
+                pass
+            else:
+                table[item_info['name']] = {'action' : 'sell',
+                                            'parent' : recipe_item_info['name'],
+                                            'count' : sub_ing['count']}
 
-        #print("Item {} {} has min sell price {}".format(itm.item_id, itm.name, cost))
-    return cost
+def find_max(recipe, item_buy_table):
+    recipe_buy_price = get_item_max_buy_price(recipe.output_id)
+    tmp = 0
+    ing = []
+    for j in recipe.input_info:
+        if type(j['value']) == LoadedRecipe:
+            ing_price, tmp_ing = find_sub_max(j['value'], item_buy_table)
+            ing.extend(tmp_ing)
+            tmp += j['count'] * ing_price
+        else:
+            ing.append(j)
+            tmp += j['count'] * get_item_max_buy_price(j['value'])
+    if tmp > recipe_buy_price:
+        return tmp, ing
+    else:
+        return recipe_buy_price, [{'value' : recipe, 'count' : 1}]
 
-def main():
+def get_obj_id(value):
+    if type(value) == LoadedRecipe:
+        return value.output_id
+    elif type(value) == dict:
+        return value['value']
 
-    if not os.path.isdir('graphs'):
-        os.makedirs('graphs')
-
-    for character_name, item_id in get_all_items():
-        graph_details = []
-        recipe_listing = []
-        #print("Checking item num {} {}".format(character_name, item_id))
-        buy_price = get_item_max_buy_price(item_id)
-        cost_price = 0
-        fees = int(buy_price * .15)
-        item = Item(item_id)
-        #if item:
-        #    print("{} Sell Value {} Cost {}".format(item.name, buy_price, cost_price + fees))
-        #else:
-        #    print("Failed to get item info for: {}".format(rep.output_id))
-        if buy_price > (cost_price + fees):
-            print("Sell that {} {} for {}".format(character_name, item.name, buy_price - fees))
-
-    for character_name, recipe_num in get_known_recipes():
-        graph_details = []
-        recipe_listing = []
-        #print("Checking recipe num: {}".format(recipe_num))
-        #count += 1
-        #if count > 10:
-        #    break
-
-        buy_price = get_recipe_max_buy_price(recipe_num)
-        cost_price = calculate_cost_profit(recipe_num, recipe_listing, graph_details)
-        fees = int(buy_price * .15)
-        rep = Recipe(recipe_num)
-        item = Item(rep.output_id)
-        #if item:
-        #    print("{} Sell Value {} Cost {}".format(item.name, buy_price, cost_price + fees))
-        #else:
-        #    print("Failed to get item info for: {}".format(rep.output_id))
-
-        if buy_price > (cost_price + fees):
-            if item:
-                print(item.name)
-            print("{} can make {}".format(character_name, recipe_num))
-            print("Buy Price {} Total cost {} fee {}".format(buy_price, cost_price, fees))
-            pprint(recipe_listing)
-            dot = Digraph(comment=recipe_num)
-            for edge in graph_details:
-                dot.edge(*edge['edge'])
-                for i in edge['edge']:
-                    if i != item.name:
-                        label = "{}".format(i)
-                        if edge['color'] == 'red':
-                            label += " : {}".format(edge['cost'])
-                        dot.node(i, color = edge['color'], label=label)
-
-            dot.render(os.path.join('graphs', item.name), view=True)
-
+def uses_mat(recipe_id, material_list, depth=1):
+    recipe = LoadedRecipe(recipe_id)
+    for i in recipe.input_info:
+        if get_obj_id(i) in material_list:
+            return True
+    else:
+        return False
 
 if __name__ == "__main__":
-    main()
-    
+    table = {}
+
+    checked_recipes = []
+    for charac, recipe_num in get_known_recipes():
+        if recipe_num not in checked_recipes and uses_mat(recipe_num, [19719]):
+            checked_recipes.append(recipe_num)
+            recipe = LoadedRecipe(recipe_num)
+            item_info = get_item_info(recipe.output_id)
+            print("Checking recipe: {}".format(item_info['name']))
+            create_table(recipe_num, table)
+    pprint(table)
+
